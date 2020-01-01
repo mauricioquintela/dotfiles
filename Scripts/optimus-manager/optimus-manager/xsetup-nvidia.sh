588f98537836b9862db62851740a73061b81e822
mauricioquintela <mfcmquintela@fc.up.pt>
2020-01-01 21:27:48 +0000

    optimus-manager

---
+++ Scripts/optimus-manager/optimus-manager/xsetup-nvidia.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# Everything you write here will be executed by the display manager when setting up the login screen in Nvidia mode.
+# (but before optimus-manager sets up PRIME with xrandr commands).
+
+xrandr --auto
