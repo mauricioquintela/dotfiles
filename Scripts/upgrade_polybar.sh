aba6c7496be844d8bfe6573c165860fff4cb6122
mauricioquintela <mfcmquintela@fc.up.pt>
2020-02-10 21:44:18 +0000

    polybar manual build due to netctl signal strength

---
+++ Scripts/upgrade_polybar.sh
@@ -0,0 +1,18 @@
+#!/bin/zsh
+
+local=$(pwd)
+cd ~/HDD/git/polybar/
+
+if {[ "$(git pull origin master | grep -c "Already up to date")" -ge 1 ]} &> /dev/null; then
+	echo "No Updates Available!"
+	cd $local
+	return
+else 
+	echo "Do you wish to update Polybar?"
+	select yn in "Yes" "No"
+	case $yn in
+		Yes ) cp ../net_nl.cpp ./src/adapters/net_nl.cpp && ./build.sh && cd $local && printf "\n\n\nUPDATED\n" && return;;
+    		No ) cd $local && echo "Update canceled. Please upgrade manually to update!" && return;;
+	esac
+fi
+return
