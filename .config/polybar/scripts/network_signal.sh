3967be6ab997b109512b3038a67da231cc8be81e
mauricioquintela <mfcmquintela@fc.up.pt>
2020-02-11 07:57:33 +0000

    updates

--- .config/polybar/scripts/network_signal.sh
+++ .config/polybar/scripts/network_signal.sh
@@ -1,5 +1,11 @@
 #!/bin/zsh
-current=$(iw dev wlp3s0 station dump | grep signal: | awk '{print $2}')
-value=`python -c "print(int(max(min((max(-90,min($current,-40))+90)/50*100, 90), 0)))"`
-echo " "$value" %"
+if [ "$(iw dev wlp3s0 station dump | grep signal: | awk '{print $2}')" ]; then
+	current=$(iw dev wlp3s0 station dump | grep signal: | awk '{print $2}')
+	value=`python -c "print(int(max(min((max(-90,min($current,-40))+90)/50*100, 90), 0)))"`
+	echo " "$value" %"
+else
+	echo "DC"
+
+fi
+
 
