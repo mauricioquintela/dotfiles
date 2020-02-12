9429087eeae869d651490fe2a4e40f02ad6d66ef
mauricioquintela <mfcmquintela@fc.up.pt>
2020-02-12 13:28:27 +0000

    updated network signal module to accept click refreshes

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -7,7 +7,7 @@ killall -q polybar
 while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
 
 # Launch bar1 and bar2
-polybar example-intel &
-polybar example-nvidia &
+polybar -r example-intel &
+polybar -r example-nvidia &
 
 echo "Bars launched..."
--- .config/polybar/scripts/network_signal.sh
+++ .config/polybar/scripts/network_signal.sh
@@ -1,11 +1,12 @@
 #!/bin/zsh
-if [ "$(iw dev wlp3s0 station dump | grep signal: | awk '{print $2}')" ]; then
-	current=$(iw dev wlp3s0 station dump | grep signal: | awk '{print $2}')
-	value=`python -c "print(int(max(min((max(-90,min($current,-40))+90)/50*100, 90), 0)))"`
-	echo " "$value" %"
-else
-	echo "DC"
-
-fi
-
-
+while true; do
+	if [ "$(iwconfig wlp3s0 | grep "Signal level" | awk '{print substr( $4, 7,8)}')" ]; then
+		current=$(iwconfig wlp3s0 | grep "Signal level" | awk '{print substr( $4, 7,8)}')
+		value=`python -c "print(int(max(min((max(-100,min($current,-40))+100)/60*100, 100), 0)))"`
+		echo " "$value" %"
+	else
+		echo "DC"
+	fi
+	sleep 60 &
+	wait
+done
