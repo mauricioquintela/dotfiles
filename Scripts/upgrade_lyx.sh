ac221d2e5c9d52f573ea56d7fe8cff66d854ef77
mauricioquintela <mfcmquintela@fc.up.pt>
2020-04-28 16:39:48 +0100

    updated lyx compilation script

--- Scripts/upgrade_lyx.sh
+++ Scripts/upgrade_lyx.sh
@@ -1,7 +1,9 @@
 #!/bin/zsh
+
 local=$(pwd)
 cd ~/HDD/git/lyx/
 need=$(ls | grep update_needed)
+sudo_need=$(ls | grep sudo_needed)
 if {[ "$(git pull origin master | grep -c "Already up to date")" -ge 1 ]}; then
 	if [ "$need" = "update_needed" ]; then
 		printf "\n\n\nUpdate in queue\n\n"
@@ -9,29 +11,49 @@ if {[ "$(git pull origin master | grep -c "Already up to date")" -ge 1 ]}; then
 		./autogen.sh
 		./configure
 		make -j4
-		notify-send -u critical -t 10000 "UPDATE DONE! WAITING FOR PASSWORD\!"
-		sudo make -j4 install
-		rm -rf update_needed
-		cd $local
-		printf "\n\n\nUPDATED\n"
-		return
+		notify-send -u critical -t 10000 "UPDATE DONE! WAITING FOR PASSWORD!"
+		if sudo make install; then
+			printf "\n\n\nUPDATED\n"
+			rm -rf update_needed
+			cd $local
+			return
+		else
+			touch sudo_needed
+			printf "\n\n\nPASSWORD NEEDED! RUN \"uplyx\" AGAIN"
+			rm -rf update_needed
+			cd $local
+			return
+		fi
 	else	
 		echo "No Updates Available!"
 		cd $local
 		return
 	fi
+elif [ "$sudo_need" = "sudo_needed" ]; then
+	notify-send -u critical -t 10000 "WAITING FOR PASSWORD!"
+	sudo make install
+	rm -rf sudo_needed
+	cd $local
+	printf "\n\n\nUPDATED\n"
+	return
+fi	
 else 
 	printf "\n\nDo you wish to update LyX?\n"
 	select yn in "Yes" "No"
 		case $yn in
-			Yes) ./autogen.sh
+			Yes) rm -rf update_needed
+				./autogen.sh
 				./configure
 				make -j4
-				notify-send -u critical -t 10000 "UPDATE DONE! WAITING FOR PASSWORD\!"
-				sudo make -j4 install
-				rm -rf update_needed
-				cd $local
-				printf "\n\n\nUPDATED\n"
+				notify-send -u critical -t 10000 "UPDATE DONE! WAITING FOR PASSWORD!"
+				if sudo make install; then
+					printf "\n\n\nUPDATED\n"
+					cd $local
+				else
+					touch sudo_needed
+					printf "\n\n\nPASSWORD NEEDED! RUN \"uplyx\" AGAIN"
+					cd $local
+				fi
 				return;;
 			No) touch update_needed
 				cd $local
